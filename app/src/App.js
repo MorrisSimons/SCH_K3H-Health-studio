2262546a3a1aa45c34746f528d553aa09ce24f8b
MorrisSimons <morris.simons01@gmail.com>
2023-02-13 05:38:23 +0100

    login data decode and done

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import  { useEffect, useState} from 'react';
+import jwt_decode from "jwt-decode";
 import './App.css';
 
+
 function App() {
+  const [ user, setUser ] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject)
+    setUser(userObject);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+    
+  }, []);
+  //if we have no user show login button
+  //if we have a user show log out button 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id= "signInDiv"></div>
+      { user &&
+        <div>
+          <img src={user.picture} alt="user profile" />
+          <h1> Welcome {user.name} </h1>
+          <h2> Email: {user.email} </h2>
+          <h2> User ID: {user.sub} </h2>
+        </div>
+      }
     </div>
   );
 }
