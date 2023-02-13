d33cf0fdabb39d4a2f607ae18f577b7f4509c81d
MorrisSimons <morris.simons01@gmail.com>
2023-02-13 06:25:28 +0100

    clean up

--- app/src/App.js
+++ app/src/App.js
@@ -1,58 +1,23 @@
-import  { useEffect, useState} from 'react';
-import jwt_decode from "jwt-decode";
+import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
-  const [ user, setUser ] = useState({});
-
-  function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    var userObject = jwt_decode(response.credential);
-    console.log(userObject)
-    setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
-    document.getElementById("profile").hidden = true;
-  }
-
-function handleSignOut(event) {
-  setUser({});
-  document.getElementById("signInDiv").hidden = false;
-}
-
-
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "",
-      callback: handleCallbackResponse,
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large"}
-    );
-    
-    google.accounts.id.prompt();
-  }, []);
-  //if we have no user show login button
-  //if we have a user show log out button 
   return (
     <div className="App">
-      <div id= "signInDiv"></div>
-      { Object.keys(user).length !== 0 &&
-        <button onClick= {(e) => handleSignOut(e)}>Sign Out</button>
-      }
-      
-      { Object.keys(user).length !== 0 &&
-        <div id='profile'>
-        <img src={user.picture} alt="user profile" />
-        <h1> Welcome {user.name} </h1>
-        <h2> Email: {user.email} </h2>
-        <h2> User ID: {user.sub} </h2>
-      </div>
-      }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
