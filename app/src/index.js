cddd4a1460c422a828192508ad9c302c94ede7d2
MorrisSimons <morris.simons01@gmail.com>
2023-02-21 21:22:45 +0100

    adding routing and changeing app.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import Login from './login_function/Login';
@@ -8,8 +9,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Login />
+    <BrowserRouter>
+      <App />
+      <Login />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
