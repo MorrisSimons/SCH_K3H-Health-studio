586a1746c70c86341e4e71719a775c8db9008158
MorrisSimons <morris.simons01@gmail.com>
2023-02-21 21:06:31 +0100

    logindashboard started

--- app/src/login_dashboard/login_dashbd.js
+++ app/src/login_dashboard/login_dashbd.js
@@ -1,9 +1,11 @@
+//import logo from './logo.svg';
+import '../login_dashboard.css';
 import  { useEffect, useState} from 'react';
 import jwt_decode from "jwt-decode";
 import config from '../config.json';
 
 
-function App() {
+function LoginDashboard() {
   const [ user, setUser ] = useState({});
 
   function handleCallbackResponse(response) {
@@ -14,11 +16,6 @@ function App() {
     document.getElementById("signInDiv").hidden = true;
     document.getElementById("profile").hidden = true;
   }
-
-function handleSignOut(event) {
-  setUser({});
-  document.getElementById("signInDiv").hidden = false;
-}
 
 
 
@@ -37,17 +34,19 @@ function handleSignOut(event) {
     google.accounts.id.prompt();
   }, []);
   //if we have no user show login button
-  //if we have a user show log out button 
-  return (
+  //if we have a user show log out button
+return (
     <div className="App">
-      <div id= "signInDiv"></div>
-      { Object.keys(user).length !== 0 &&
-        <button onClick= {(e) => handleSignOut(e)}>Sign Out</button>
-        
-      }
-
+    { Object.keys(user).length !== 0 &&
+        <div id='profile'>
+        <img src={user.picture} alt="user profile" />
+        <h1> Welcome {user.name} </h1>
+        <h2> Email: {user.email} </h2>
+        <h2> User ID: {user.sub} </h2>
     </div>
-  );
+    }
+    </div>
+);
 }
 
-export default App;
+export default LoginDashboard;
