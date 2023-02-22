71d833e1e6390530aa07b1d60d86a9987f324ee5
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2023-02-22 22:34:25 +0100

    Removed unneeded link

--- app/src/views.jsx
+++ app/src/views.jsx
@@ -1,19 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import NotFound from "./components/NotFound";
-import Login from "./login_function/Login";
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import NotFound from "./components/NotFound"
+import Login from "./login_function/Login"
+import Index from "./components/Index"
 
 const Views = () => {
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+	const isLoggedIn = localStorage.getItem("isLoggedIn") === "true"
 
-  return (
-    <Routes>
-      <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
+	return (
+		<Routes>
+			<Route path="/" element={isLoggedIn ? <Home /> : <Index />} />
+			<Route path="/about" element={<About />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	)
+}
 
-export default Views;
+export default Views
