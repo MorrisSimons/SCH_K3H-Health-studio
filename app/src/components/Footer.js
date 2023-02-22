ecb9680885509077fe8b6916ab0b2edb1374669b
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2023-02-22 22:34:47 +0100

    Added footer

---
+++ app/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./Footer.css"
+
+function Footer() {
+	return (
+		<div className="footer">
+			<div id="footerText" className="footerText">
+				<p>
+					© 2021, The Not Logged In Dashboard|
+					<Link to="/about">About us</Link>|
+					<Link to="/contact">Contact us </Link>|
+					<Link to="/policy">Privacy policy </Link>|
+					<Link to="/terms">Terms of service </Link>|
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default Footer
