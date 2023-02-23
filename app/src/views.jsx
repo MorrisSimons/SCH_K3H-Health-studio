e83dd139e273aeef6d809873b8c70ca883e34d9a
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2023-02-23 10:49:06 +0100

    Added Images

--- app/src/views.jsx
+++ app/src/views.jsx
@@ -5,19 +5,19 @@ import NotFound from "./components/NotFound"
 import LoginDashboard from "./components/LoginDashboard"
 
 const Views = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  const isLoggedIn = user && Object.keys(user).length !== 0;
+	const user = JSON.parse(localStorage.getItem("user"))
+	const isLoggedIn = user && Object.keys(user).length !== 0
 
-  return (
-    <Routes>
-      <Route path="/" element={isLoggedIn ? <LoginDashboard /> : <Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<h1>Contact Us</h1>} />
-      <Route path="/policy" element={<h1>Privacy Policy</h1>} />
-      <Route path="/terms" element={<h1>Terms of Service</h1>} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  )
+	return (
+		<Routes>
+			<Route path="/" element={isLoggedIn ? <LoginDashboard /> : <Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/contact" element={<h1>Contact Us</h1>} />
+			<Route path="/policy" element={<h1>Privacy Policy</h1>} />
+			<Route path="/terms" element={<h1>Terms of Service</h1>} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	)
 }
 
 export default Views
