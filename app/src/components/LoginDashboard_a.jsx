9bd9c735bcacb98826989ff6a4094923596e899a
MorrisSimons <morris.simons01@gmail.com>
2023-02-25 06:43:08 +0100

    login dashboard template for admin and coach

---
+++ app/src/components/LoginDashboard_a.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./LoginDashboard_a.css"
+import { Link } from "react-router-dom";
+
+function LoginDashboard() {
+  function handleSignOut() {
+    localStorage.removeItem("user");
+    window.location.reload();
+  }
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log("user: " + user);
+
+  const data = [
+    {
+      id: 1,
+      title: "Gör test",
+      navigation: "/do_test",
+    },
+    {
+      id: 2,
+      title: "Se data",
+      navigation: "/se_data",
+    },
+    {
+      id: 3,
+      title: "Analyser",
+      navigation: "/doanalysis",
+    },
+    {
+      id: 4,
+      title: "Skapa formulär",
+      navigation: "/createform",
+    },
+    {
+      id: 5,
+      title: "formulär",
+      navigation: "/forms",
+    },
+    {
+      id: 6,
+      title: "Se lag",
+      navigation: "/se_lag",
+    },
+  ];
+
+  return (
+    <section className="dashboard__section_admin">
+      <div id="admin">
+        <Header />
+        <h1>LoginDashboard</h1>
+        <div className="container dashboard__container">
+          {data.map(({ id, title, navigation }) => {
+            return (
+              <Link to={navigation} key={id}>
+                <button className="dashboard__button">{title}</button>
+              </Link>
+            );
+          })}
+        </div>
+        <button className="logout" onClick={handleSignOut}>Logga ut</button>
+        <h1>Admin</h1>
+        <p>{user?.name}</p>
+        <p>{user?.email}</p>
+        <Footer />
+      </div>
+    </section>
+  );
+}
+
+export default LoginDashboard;
---
+++ app/src/components/LoginDashboard_c.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./LoginDashboard_c.css";
+import { Link } from "react-router-dom";
+
+function LoginDashboard() {
+  function handleSignOut() {
+    localStorage.removeItem("user");
+    window.location.reload();
+  }
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log("user: " + user);
+
+  const data = [
+    {
+      id: 1,
+      status: "active",
+      name: "Joe Doe",
+      navigation: "/joe_doe_coach1",
+    },
+    {
+      id: 2,
+      status: "inactive",
+      name: "John Smith",
+      navigation: "/john_smith_coach2",
+    },
+    {
+      id: 3,
+      status: "active",
+      name: "Emma Johnson",
+      navigation: "/emma_johnson_coach2",
+    },
+    {
+      id: 4,
+      status: "inactive",
+      name: "Michael Brown",
+      navigation: "/michael_brown_coach2",
+    },
+    {
+      id: 5,
+      status: "pending",
+      name: "Sarah Lee",
+      navigation: "/sarah_lee_coach2",
+    },
+    {
+      id: 6,
+      status: "pending",
+      name: "David Garcia",
+      navigation: "/david_garcia_coach2",
+    },
+    {
+      id: 7,
+      status: "active",
+      name: "Emily Davis",
+      navigation: "/emily_davis_coach2",
+    },
+  ];
+
+  return (
+    <section className="dashboard__section_admin">
+        <h1>LoginDashboard</h1>
+
+    <div className="panel-container">
+        <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+    </div>
+
+    <Link to="/se_data"><button>Se data</button></Link>
+    <Link to="/analys"><button>analys</button></Link>
+    <Link to="/add_user"><button>Lägg till spelare</button></Link>
+    <button onClick={handleSignOut}>Logga ut</button>
+
+    </div>
+
+        
+
+        <div className="container dashboard__container">
+          {data.map(({ id, status, name, navigation }) => {
+            return (
+              <Link key={id} to={navigation}>
+                <button className={`dashboard__article dashboard__article--${status}`}>
+                  <h2>{name}</h2>
+                </button>
+              </Link>
+            );
+          })}
+        </div>
+        <h1>Admin</h1>
+        <p>{user?.name}</p>
+        <p>{user?.email}</p>
+        <button onClick={handleSignOut}>Sign Out</button>
+        <Footer />
+    </section>
+  );
+}
+
+export default LoginDashboard;
