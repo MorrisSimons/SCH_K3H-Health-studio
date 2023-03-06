52b4546bba088f7f0b19dee4c395e89c30e0ee6c
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2023-03-06 11:09:48 +0100

    Added test cases to the server

---
+++ server.test.js
@@ -0,0 +1,20 @@
+// Test if the server is up
+
+const { query } = require("express")
+
+function testAddUser() {
+	// Create a new user
+	const user = {
+		email: "test@test.test",
+	}
+	// Send the user to the server
+	query = fetch("/api/addUser", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(user),
+	}).then((response) => {
+		console.log(response)
+	})
+}
