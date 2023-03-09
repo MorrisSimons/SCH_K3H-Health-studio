a7d78e0648087839fac5efc5200ee52da34c0963
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2023-03-09 09:32:29 +0100

    Cleaned the codebase

--- api/controllers/controllers.js
+++
@@ -1,7 +0,0 @@
-const saySomething = (req, res, next) => {
-	res.status(200).json({
-		body: "Hello from the server!",
-	})
-}
-
-module.exports.saySomething = saySomething
--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 // Import dependencies
 const express = require("express")
-const sqlite3 = require("sqlite3").verbose()
+const db = require("./persistence")
 
 // Import API components
-const controller = require("./api/controllers/controllers")
+const addUser = require("./api/addUser")
+const deleteUser = require("./api/deleteUser")
+const getUsers = require("./api/getUsers")
 
 //--------------------------------
 // Note app in exspress is diffrent from the app folder in react
 //--------------------------------
-
-// Import our database
-const db = new sqlite3.Database("db/k3h.sqlite3", (err) => {
-	if (err) {
-		console.error(err.message)
-	}
-	console.log("Connected to the k3h database.")
-})
 
 // Create a new express application named 'app'
 const app = express()
 
 // Set our backend port to be either an environment variable or port 5000
 const port = process.env.PORT || 5000
+
+// Import our database
+db.init()
+	.then(() => {
+		app.listen(port, () => console.log("Listening on port ${port}"))
+	})
+	.catch((err) => {
+		console.error(err)
+		process.exit(1)
+	})
 
 // This application level middleware prints incoming requests to the servers console, useful to see incoming requests
 app.use((req, res, next) => {
@@ -37,36 +41,9 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "app/build")))
 }
 
-app.post("/api/addUser", (req, res) => {
-	const sql = "INSERT INTO user (email) VALUES (?)"
-	const params = ["test2@test2.test7"]
-
-	db.run(sql, params, function (err, result) {
-		if (err) {
-			res.status(400).json({ error: err.message })
-			return
-		}
-		res.json({
-			message: "success",
-			data: params,
-		})
-	})
-})
-
-app.get("/api/getUsers", (req, res) => {
-	getUsers()
-		.then((users) => {
-			res.json({
-				message: "success",
-				data: users,
-			})
-		})
-		.catch((error) => {
-			res.status(400).json({ error: error.message })
-		})
-})
-
-app.get("/api/saySomething", controller.saySomething)
+app.get("/api/getUsers", getUsers)
+app.post("/api/addUser", addUser)
+app.delete("/api/deleteUser", deleteUser)
 
 // Catch any bad requests
 app.get("*", (req, res) => {
@@ -77,12 +54,3 @@ app.get("*", (req, res) => {
 
 // Configure our server to listen on the port defiend by our port variable
 app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`))
-
-async function getUsers() {
-	return new Promise((resolve, reject) => {
-		db.all("SELECT * FROM user", (err, rows) => {
-			if (err) return reject(err)
-			resolve(rows)
-		})
-	})
-}
