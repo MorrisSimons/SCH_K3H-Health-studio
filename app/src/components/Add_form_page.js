76da956a656a63653ade419c5f81d0fc9a4abe74
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2023-03-16 22:34:19 +0100

    Fixed styling

--- app/src/components/Add_form_page.js
+++ app/src/components/Add_form_page.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './Add_form_page.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./Add_form_page.css"
 import Header from "./Header"
 import Footer from "./Footer"
-import Add_form from './Add_form';
-
-
+import Add_form from "./Add_form"
 
 function Add_form_page() {
 	return (
-		<div className='add_form_body'>
-      <React.StrictMode>
-			<Header />
-        <Add_form/>
-			<Footer />
-      </React.StrictMode>
+		<div className="add_form_body">
+			<React.StrictMode>
+				<Header />
+				<center>
+					<Add_form />
+				</center>
+				<Footer />
+			</React.StrictMode>
 		</div>
 	)
 }
 
 export default Add_form_page
-
-
