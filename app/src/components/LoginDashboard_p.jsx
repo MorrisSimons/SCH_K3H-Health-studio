980dcbc559fc0a699eb5d49073f91927bc76ffc4
profs3t <theodor.maran@gmail.com>
2023-04-05 14:49:31 +0200

    Updated player dashboard
    Fixed layout

--- app/src/components/LoginDashboard_p.jsx
+++ app/src/components/LoginDashboard_p.jsx
@@ -23,10 +23,13 @@ function LoginDashboard() {
 		{
 			id: 2,
 			title: "Se data",
-			navigation: "/data",
+			navigation: "/see_data",
 		},
+	]
+
+	const load_more = [
 		{
-			id: 3,
+			id: 1,
 			title: "Ladda fler tester",
 			navigation: "/load_more_tests",
 		}
@@ -36,74 +39,96 @@ function LoginDashboard() {
 		{
 			id: 1,
 			title: "Test 1",
-			navigation: "/see_analysis",
+			navigation: "/test1",
 		},
 		{
 			id: 2,
 			title: "Test 2",
-			navigation: "/data",
+			navigation: "/test2",
 		},
 		{
 			id: 3,
 			title: "Test 3",
-			navigation: "/load_more_tests",
+			navigation: "/test3",
 		},
 		{
 			id: 4,
 			title: "Test 4",
-			navigation: "/load_more_tests",
+			navigation: "/test4",
+		},
+		{
+			id: 5,
+			title: "Test 5",
+			navigation: "/test5",
+		},
+		{
+			id: 6,
+			title: "Test 6",
+			navigation: "/test6",
+		},
+		{
+			id: 7,
+			title: "Test 7",
+			navigation: "/test7",
 		}
-		
 	]
 
 	return (
-		<div>
+		<section className="dashboard__section_player">
 			<Header />
-			<h1 className="title">Login Dashboard Player</h1>
-			<section className="dashboard__section_player">
-			<section className="tests">
-				<h2>Senaste tester</h2>
-				<div className="test_buttons">
-					{test_buttons.map(({ id, title, navigation }) => {
-						return (
-							<Link to={navigation} key={id}>
-								<button className="dashboard__button">{title}</button>
-							</Link>
-						)
-					})}
-				</div>
-			</section>
-			
-				<section className="playerInfo">
-					<img className="playerPicture" src={icon} alt="icon" />
-					<div className="info">
-						<p>Coach</p>
-						<p>Tel.nr.</p>
-						<p>Mail</p>
-						<p>Ålder</p>
-						<p>Pers.nr.</p>
-						<p>Adress</p>
-						<p>Antal tester</p>
-						<p>Senaste testet</p>
-						<p>Sport</p>
+			<h1 className="title">Player Dashboard</h1>
+				<div className="main_layout">
+					<div className="playerInfo">
+						<img className="playerPicture" src={icon} alt="icon" />
+						<div className="info">
+							<p>Coach</p>
+							<p>Tel.nr.</p>
+							<p>Mail</p>
+							<p>Ålder</p>
+							<p>Pers.nr.</p>
+							<p>Adress</p>
+							<p>Antal tester</p>
+							<p>Senaste testet</p>
+							<p>Sport</p>
+						</div>
+						<h2 className="playerName">John Doe</h2>
 					</div>
-					<h2 className="playerName">John Doe</h2>
-				</section>
-			<div className="side_buttons">
-				{side_buttons.map(({ id, title, navigation }) => {
-					return (
-						<Link to={navigation} key={id}>
-							<button className="dashboard__button">{title}</button>
-						</Link>
-					)
-				})}
-			<button className="logout" onClick={handleSignOut}>
-				Logga ut
-			</button>
-			</div>
-			</section>
+					<div className="tests">
+						<h2>Senaste tester</h2>
+						<div className="test_buttons">
+							{test_buttons.map(({ id, title, navigation }) => {
+								return (
+									<Link to={navigation} key={id}>
+										<button className="dashboard__button">{title}</button>
+									</Link>
+								)
+							})}
+						</div>
+						<div className="load_button">
+							{load_more.map(({ id, title, navigation }) => {
+								return (
+									<Link to={navigation} key={id}>
+										<button className="dashboard__button">{title}</button>
+									</Link>
+								)
+							})}
+						</div>
+					</div>
+					<div className="side_buttons">
+						{side_buttons.map(({ id, title, navigation }) => {
+							return (
+								<Link to={navigation} key={id}>
+									<button className="dashboard__button">{title}</button>
+								</Link>
+							)
+						})}
+						<button className="dashboard__button" onClick={handleSignOut}>
+							Logga ut
+						</button>
+					</div>
+				</div>
 			<Footer />
-		</div>
+		</section>
 	)
 }
 
