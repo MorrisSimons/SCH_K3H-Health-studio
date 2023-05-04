4d5f356072a6c97a338b7001fedc9fd76a923b09
Uplink036 <Oliversjodin02@gmail.com>
2023-05-04 09:40:32 +0000

    Fixed for demo4

--- api/getUserType.js
+++ api/getUserType.js
@@ -3,14 +3,16 @@ const db = require("../db")
 module.exports = async (req, res) => {
 	try {
         if (!req.body.email) {
-            res.status(400).send("Missing email")
+            res.status(400).send({"Fault":"Missing email"})
         }
 		email = req.body.email
 		const accountType = await db.getUserType(email)
 		if (accountType.length === 0) {
-			res.status(400).send("No account found")
+			res.status(400).send({"Fault":"No account found"})
 		}
-		res.send(accountType)
+		else {
+			res.send(accountType)
+		}
 	} catch (err) {
 		console.log(err)
 		res.status(500).send(err)
